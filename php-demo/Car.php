01817814113c503b960f968b67b29ef3e027b6bd
DucLangTu <77925620+ducvo177@users.noreply.github.com>
2023-04-04 11:12:30 +0700

    $this_variable

--- php-demo/Car.php
+++ php-demo/Car.php
@@ -4,13 +4,18 @@ class Car{
     public $color;
 
     public function __construct($name, $color){
-        $this->name= $name;
+        $this->name= $name; 
         $this->color= $color;
     }
 
     public function introduce() { //Ví dụ method introduce được định nghĩa để đại diện cho một hành động mà class có thể thực hiện 
-    echo "This " . $this->name . " car has " . $this->color . " color.";
-  }
+    echo "This " . $this->name . " car has " . $this->color . " color.\n";
+    }
+
+    public function getName(){
+        echo "Name of the car is " . $this->name;// $this là một biến đặc biệt được sử dụng để truy cập vào property và method của class hiện tại(Car)
+    }
 }
 $mercedes = new Car("Mercedes","green");
 $mercedes->introduce(); 
+$mercedes->getName();
