92403e217138a1c8d21d75fcaac8c08a405211d4
DucLangTu <77925620+ducvo177@users.noreply.github.com>
2023-04-05 09:48:39 +0700

    Dependency-injection

--- php-demo/Clone.php
+++ php-demo/Clone.php
@@ -9,9 +9,23 @@ class Car{
     }
 
     public function getName(){
-        echo "Name of the car is " . $this->name;
+        echo "Name of the car is " . $this->name.PHP_EOL;
+    }
+}
+
+class Vehicle{
+    private $car;
+    public function __construct(Car $car) { //  inject đối tượng Car vào khi khởi tạo Vehicle
+        $this->car = $car;
+    }
+
+    public function logName() {
+        $this->car->getName();// Tái sử dụng các hàm bên trong của class Car
     }
 }
 $mercedes = new Car("Mercedes","green");
+$vehicle = new Vehicle($mercedes);// Dễ dàng thay đổi một Car khác cần inject vào Vehicle
+$vehicle->logName();
+
 $mercedes2 = clone $mercedes; //Clone lại object $mercedes bao gồm các property và method của nó
 $mercedes2->getName();
