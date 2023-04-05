3d75eaab51db30f18cc3652dc27fd4b327c90154
DucLangTu <77925620+ducvo177@users.noreply.github.com>
2023-04-05 10:48:42 +0700

    Where

--- mysql-demo/Insert.sql
+++ mysql-demo/Insert.sql
@@ -1,1 +1,9 @@
-INSERT INTO users (id, name, created_at) VALUES (1, 'John Doe', CURRENT_TIMESTAMP);
+INSERT INTO users (id, name, created_at) VALUES 
+(1, 'John Doe', '2023-04-05 09:00:00'),
+(2, 'Jane Smith', '2023-04-05 09:01:00'),
+(3, 'Bob Johnson', '2023-04-05 09:02:00');
+
+INSERT INTO books (id, name, user_id) VALUES 
+(1, 'The Great Gatsby', 1),
+(2, 'To Kill a Mockingbird', 2),
+(3, 'Pride and Prejudice', 3);
