39f27151827e406f2ccfc372b642c1a68f486ee3
DucLangTu <77925620+ducvo177@users.noreply.github.com>
2023-04-06 15:51:34 +0700

    Watcher

---
+++ vue-demo/Watcher.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+      <p>{{ message }}</p>
+      <input v-model="inputMessage">
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    data() {
+      return {
+        message: "Hello World!",
+        inputMessage: ""
+      }
+    },
+    watch: {
+      inputMessage(newVal) {
+        this.message = newVal.toUpperCase();
+      }
+    }
+  }
+  </script>
