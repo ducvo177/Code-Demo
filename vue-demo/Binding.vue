7852698fc636ff78b77dfc02b33a6192df0c6e73
DucLangTu <77925620+ducvo177@users.noreply.github.com>
2023-04-06 15:14:10 +0700

    Data_Binding

---
+++ vue-demo/Binding.vue
@@ -0,0 +1,26 @@
+<template>
+    <div>
+     <h1 :class="class" :style="{backgroundColor:color}">{{ message }}</h1>
+     <form>
+    <label for="email">Email:</label>
+    <input type="email" id="email" v-model="email">
+    <!-- Two way binding -->
+    <button type="submit">Submit</button>
+  </form>
+  <p>{{ email }}</p>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    data() {
+      return {
+        email:"",
+        class:'title',
+        message: "Welcome to Concrete",
+        color:"red",
+      };
+    },
+  };
+  </script>
+  
