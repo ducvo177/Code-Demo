f064ca7a971880d583b28b3cc7b2310d9209aa39
DucLangTu <77925620+ducvo177@users.noreply.github.com>
2023-04-06 15:02:55 +0700

    Template

---
+++ vue-demo/Template.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <p v-if="check" :title="message">{{ message }}</p>
+    <button @click="handleHide">Hide Message</button>
+    <ul>
+      <li v-for="item in items" :key="item.id">{{ item.name }}</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  methods: {
+    handleHide: function () {
+      this.check = !this.check;
+    },
+  },
+  data() {
+    return {
+      check: true,
+      message: "Welcome to Concrete",
+      items: [
+        { id: 1, name: "Item 1" },
+        { id: 2, name: "Item 2" },
+        { id: 3, name: "Item 3" },
+      ],
+    };
+  },
+};
+</script>
