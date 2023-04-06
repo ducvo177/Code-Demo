40b7b24d5e3a2a0554c7774061f2e43e7241ddab
DucLangTu <77925620+ducvo177@users.noreply.github.com>
2023-04-06 15:24:18 +0700

    Event_handling

---
+++ vue-demo/Event_Handling.vue
@@ -0,0 +1,36 @@
+<template>
+    <div>
+      <p v-if="check" :title="message">{{ message }}</p>
+      <button @click="handleHide">Hide Message</button>
+      <form @submit="handleSubmit">
+    <label for="email">Email:</label>
+    <input @input="handleChange" type="email" id="email" v-model="email">
+    <button type="submit">Submit</button>
+  </form>
+  <h1>{{ email }}</h1>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    methods: {
+      handleHide: function () {
+        this.check = !this.check;
+      },
+      handleChange: function () {
+        console.log('email input is changing')
+      },
+      handleSubmit: function () {
+        this.email=""
+      }
+    },
+    data() {
+      return {
+        check: true,
+        message: "Welcome to Concrete",
+        email:""
+      };
+    },
+  };
+  </script>
+  
