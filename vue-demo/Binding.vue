dc5be0d5bf3c656bd5340549f735a370bc82a038
DucLangTu <77925620+ducvo177@users.noreply.github.com>
2023-04-07 09:06:20 +0700

    Binding

--- vue-demo/Binding.vue
+++ vue-demo/Binding.vue
@@ -1,26 +1,29 @@
 <template>
-    <div>
-     <h1 :class="class" :style="{backgroundColor:color}">{{ message }}</h1>
-     <form>
-    <label for="email">Email:</label>
-    <input type="email" id="email" v-model="email">
-    <!-- Two way binding -->
-    <button type="submit">Submit</button>
-  </form>
-  <p>{{ email }}</p>
-    </div>
-  </template>
-  
-  <script>
-  export default {
-    data() {
-      return {
-        email:"",
-        class:'title',
-        message: "Welcome to Concrete",
-        color:"red",
-      };
-    },
-  };
-  </script>
-  
+  <div>
+    <h1 :class="class" :style="{ backgroundColor: color }">{{ message }}</h1>
+    <form>
+      <label for="email">Email:</label>
+      <input type="email" id="email" v-model="email"/>
+      <!-- Two way binding -->
+      <button type="submit">Submit</button>
+    </form>
+   
+  </div>
+</template>
+
+<script>
+import { reactive } from "vue";
+export default {
+  setup() {
+    const state= reactive({
+      email: "",
+      class: "title",
+      message: "Welcome to Concrete",
+      color: "red",
+    });
+    return {
+      ...state,
+    }
+  },
+};
+</script>
